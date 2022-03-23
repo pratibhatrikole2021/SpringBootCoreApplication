c1a2c7e05a21861ec72e345bef5fe2186652adca
pratibhatrikole2021 <pratibhatrikole1998@gmail.com>
2022-03-23 03:35:09 -0700

    show data

--- SpringBootCoreApp/src/main/java/com/csi/controller/ApplicationController.java
+++ SpringBootCoreApp/src/main/java/com/csi/controller/ApplicationController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.LinkedList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class ApplicationController {
@@ -26,5 +29,44 @@ public class ApplicationController {
         return "HR";
 
     }
+
+    @GetMapping("/showdata")
+
+    public List<Employee> showData(){
+
+        List<Employee> empList = new LinkedList<>();
+        empList.add(new Employee(1,"pratibha"));
+        empList.add(new Employee(2,"pooja"));
+
+        return empList;
+
+
+    }
+
+
+
+}
+
+class Employee{
+
+    private int empId;
+
+    private String empName;
+
+    public Employee(int empId, String empName) {
+        this.empId = empId;
+        this.empName = empName;
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+
+
 
 }
